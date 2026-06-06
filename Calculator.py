c14bee02c7d9e54809f705cb8f573b13d6c8b302
maryumcoder <salmanmaryum873@gmail.com>
2026-06-06 23:35:00 +0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,11 @@
+num1 = float(input("Pehla number likho: "))
+num2 = float(input("Doosra number likho: "))
+
+print("Jama:", num1 + num2)
+print("Minus:", num1 - num2)
+print("Zarb:", num1 * num2)
+
+if num2 != 0:
+    print("Taqseem:", num1 / num2)
+else:
+    print("Zero se taqseem nahi ho sakti")
